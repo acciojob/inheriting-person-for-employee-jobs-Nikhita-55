71c5d48a9fbefb81208af61f2b23a529b43db8ab
NIKHITA PARDHE <153489800+Nikhita-55@users.noreply.github.com>
2024-09-23 22:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+constructor(name,age){
+	this.name=name;
+	this.age=age;
+}
+	 getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+// Employee class extends Person
+class Employee extends Person {
+    constructor(name, age, jobTitle, salary) {
+        super(name, age); // Call the parent class constructor
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    // Method to get the employee's details (extends getDetails from Person)
+    getDetails() {
+        return `${super.getDetails()}, Job Title: ${this.jobTitle}, Salary: ${this.salary}`;
+    }
+}
+	
+}
 
 function Employee(name, age, jobTitle) {}
 
